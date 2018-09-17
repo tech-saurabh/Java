07847c156023cdf99d8b06dff46cc3f6918c007d
Saurabh Tripathi <saurabhtripathi9999@gmail.com>
2018-09-17 22:44:08 +0530

    first java program added
    java program to print hello world.

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld 
+{
+	public static void main(String[] args)         // main method 
+	{
+		System.out.println("hello world");     // using predefined function in java to print
+	}
+}
