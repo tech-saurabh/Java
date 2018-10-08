94f1176cd8987c978910b7408db9eef7eb84dbbd
Saurabh Tripathi <saurabhtripathi9899@gmail.com>
2018-10-08 22:27:03 +0530

    Switch example in java

---
+++ Switch.java
@@ -0,0 +1,20 @@
+public class Switch {
+    static void switch_method() //switch method  in java
+    {
+        int happy=3;
+       switch(happy)
+       {
+           case 1:System.out.println("happy coding 1");
+           break;
+           case 2:System.out.println("happpy coding 2");
+           break;
+           case 3:System.out.println("happy coding 3");
+           break;
+       }
+    }
+
+    public static void main(String[] args) {
+        switch_method();
+    }
+    
+}
